15480e12716b1b3ece1cbc4ac1de00c9237636ee
romeoalpharomeo <ryanrey0333@gmail.com>
2021-04-25 16:57:36 -0400

    Improved fibonacci.

---
+++ ArrayChallenges/ShortestFibonacci.js
@@ -0,0 +1,12 @@
+function fibonacciArray(n) {
+    // the [0, 1] are the starting values of the array to calculate the rest from
+    var fibArr = [0, 1];
+    for (let i = 1; i < n - 1; i++){
+        fibArr.push(fibArr[i] + fibArr[i-1]);
+
+    }
+    return fibArr;
+}
+
+var result = fibonacciArray(10);
+console.log(result); // we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
