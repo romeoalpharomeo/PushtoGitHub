2a8b6ed19f5db7352e824b8d0a291d4dd20e8f34
romeoalpharomeo <ryanrey0333@gmail.com>
2021-04-27 19:06:00 -0400

    Making connections complete.

---
+++ Profile PageMakingConnections/script.js
@@ -0,0 +1,8 @@
+function changeName() {
+    document.querySelector("#username").innerHTML = "Tom Hardy";
+}
+
+function removeRequest() {
+    document.querySelector(".request1").remove(".request1");
+
+}
