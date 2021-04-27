7fd7a5b387ddc5b14c9af6299604f571b1099699
romeoalpharomeo <ryanrey0333@gmail.com>
2021-04-27 16:55:00 -0400

    Likes complete.

---
+++ Likes/script.js
@@ -0,0 +1,17 @@
+var clicks1 = 0;
+function countLikes1() {
+    clicks1 += 1;
+    document.querySelector("#numberOfLikes1").innerHTML = clicks1 + " Like(s)";
+}
+
+var clicks2 = 0;
+function countLikes2() {
+    clicks2 += 1;
+    document.querySelector("#numberOfLikes2").innerHTML = clicks2 + " Like(s)";
+}
+
+var clicks3 = 0;
+function countLikes3() {
+    clicks3 += 1;
+    document.querySelector("#numberOfLikes3").innerHTML = clicks3 + " Like(s)";
+}
