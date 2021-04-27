2717a5159ce88c73ef3c5d645b5a0c5da4e5d31b
romeoalpharomeo <ryanrey0333@gmail.com>
2021-04-27 08:25:09 -0400

    Video Preview completed.

---
+++ AlgoPracticeAll/hoverevent.js
@@ -0,0 +1,6 @@
+function addShadow(element) {
+    element.classList.add("shadow");
+}
+function removeShadow(element) {
+    element.classList.remove("shadow")
+}
---
+++ VideoPreview/script.js
@@ -0,0 +1,9 @@
+// var x = document.getElementById("vid"); 
+
+// function stopPreview() { 
+//     x.pause(); 
+// }
+
+// function playPreview() {
+//     x.play();    
+// }    
