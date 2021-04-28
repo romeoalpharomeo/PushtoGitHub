b28cee9ad1b9d199485ba5d0fc51cf5653ea975f
romeoalpharomeo <ryanrey0333@gmail.com>
2021-04-27 22:49:15 -0400

    done.

---
+++ DojoWeather/script.js
@@ -0,0 +1,10 @@
+function removeCookieMessage() {
+        document.querySelector(".cookies").remove(".cookies");
+}
+
+// function convertToFahrenheit() {
+
+//     var valResult = document.getElementById("changetemp1").value; // get the value of the field
+//     var results = parseInt(valResult) + mod; // convert the value to int to do calculation
+//     console.log(results);
+// }
